55fd95d096212c4bdae54e607bd154a97758b55e
David Gray <grayda@solidinc.org>
2015-08-20 20:39:34 +1000

    Revert "Revert "Code cleanup & Kp data addition""
    This reverts commit 9cd98c201eb5a7035bcf18c754eb025ce64f06d9.

--- aurora.go
+++ aurora.go
@@ -20,63 +20,66 @@ var SpeedAlert = 500.0
 var DensityAlert = 10.0
 var MissingData = -999.9
 
-// /pub/lists//wingkp/wingkp_list.txt
-
 // Get will connect to the Space Weather site and download the latest 1 minute reports from the ACE spacecraft. It will then parse the single MOST RECENT data
 // put it into a map of map[string]float64, then return the results.
 func Get() map[string]float64 {
 	var results map[string]float64 = make(map[string]float64)
 	// Create a new FTP object and connect to the SWPC site
+
+	// Retrieve the latest ACE data from this folder
+
+	aceMag := getFile("/pub/lists/ace/ace_mag_1m.txt")
+	results["Long"], _ = strconv.ParseFloat(aceMag[len(aceMag)-2], 64)
+	results["Lat"], _ = strconv.ParseFloat(aceMag[len(aceMag)-3], 64)
+	results["Bt"], _ = strconv.ParseFloat(aceMag[len(aceMag)-4], 64)
+	results["Bz"], _ = strconv.ParseFloat(aceMag[len(aceMag)-5], 64)
+	results["By"], _ = strconv.ParseFloat(aceMag[len(aceMag)-6], 64)
+	results["Bx"], _ = strconv.ParseFloat(aceMag[len(aceMag)-7], 64)
+	results["S"], _ = strconv.ParseFloat(aceMag[len(aceMag)-8], 64)
+	results["Seconds"], _ = strconv.ParseFloat(aceMag[len(aceMag)-9], 64)
+	results["JulianDay"], _ = strconv.ParseFloat(aceMag[len(aceMag)-10], 64)
+	results["Time"], _ = strconv.ParseFloat(aceMag[len(aceMag)-11], 64)
+	results["DA"], _ = strconv.ParseFloat(aceMag[len(aceMag)-12], 64)
+	results["MO"], _ = strconv.ParseFloat(aceMag[len(aceMag)-13], 64)
+	results["YR"], _ = strconv.ParseFloat(aceMag[len(aceMag)-14], 64)
+
+	// Retrieve the latest ACE data from this folder
+
+	aceSwepam := getFile("/pub/lists/ace/ace_swepam_1m.txt")
+	results["IonTemperature"], _ = strconv.ParseFloat(aceSwepam[len(aceSwepam)-2], 64)
+	results["Speed"], _ = strconv.ParseFloat(aceSwepam[len(aceSwepam)-3], 64)
+	results["Density"], _ = strconv.ParseFloat(aceSwepam[len(aceSwepam)-4], 64)
+
+	wingKp := getFile("/pub/lists/wingkp/wingkp_list.txt")
+	results["Kp"], _ = strconv.ParseFloat(wingKp[len(wingKp)-2], 64)
+	results["Kp1hour"], _ = strconv.ParseFloat(wingKp[len(wingKp)-4], 64)
+	results["Kp4hours"], _ = strconv.ParseFloat(wingKp[len(wingKp)-10], 64)
+
+	return results
+}
+
+func getFile(file string) []string {
 	ftp, _ := goftp.Connect("ftp.swpc.noaa.gov:21")
 	// Log in as an anonymous user
 	ftp.Login("anonymous", "")
-	// Retrieve the latest ACE data from this folder
-	_, _ = ftp.Retr("/pub/lists/ace/ace_mag_1m.txt", func(r io.Reader) error {
-		// Make a new buffer
-		buf := new(bytes.Buffer)
-		// And read the data from the file
-		buf.ReadFrom(r)
-		// This regex searches for consecutive spaces, as our data is space-separated (no pun intended)
-		reg := regexp.MustCompile("\\s+")
-		// Split our returned data into a string array
-		data := reg.Split(buf.String(), -1)
-		// Now we just need to get the last couple of groups of data. The last result will always be empty, so it's ignored
-
-		results["Long"], _ = strconv.ParseFloat(data[len(data)-2], 64)
-		results["Lat"], _ = strconv.ParseFloat(data[len(data)-3], 64)
-		results["Bt"], _ = strconv.ParseFloat(data[len(data)-4], 64)
-		results["Bz"], _ = strconv.ParseFloat(data[len(data)-5], 64)
-		results["By"], _ = strconv.ParseFloat(data[len(data)-6], 64)
-		results["Bx"], _ = strconv.ParseFloat(data[len(data)-7], 64)
-		results["S"], _ = strconv.ParseFloat(data[len(data)-8], 64)
-		results["Seconds"], _ = strconv.ParseFloat(data[len(data)-9], 64)
-		results["JulianDay"], _ = strconv.ParseFloat(data[len(data)-10], 64)
-		results["Time"], _ = strconv.ParseFloat(data[len(data)-11], 64)
-		results["DA"], _ = strconv.ParseFloat(data[len(data)-12], 64)
-		results["MO"], _ = strconv.ParseFloat(data[len(data)-13], 64)
-		results["YR"], _ = strconv.ParseFloat(data[len(data)-14], 64)
+	var data []string
+	_, _ = ftp.Retr(file, func(r io.Reader) error {
+		data = extractData(r)
 		return nil
 	})
+	return data
+}
 
-	// Retrieve the latest ACE data from this folder
-	_, _ = ftp.Retr("/pub/lists/ace/ace_swepam_1m.txt", func(r io.Reader) error {
-		// Make a new buffer
-		buf := new(bytes.Buffer)
-		// And read the data from the file
-		buf.ReadFrom(r)
-		// This regex searches for consecutive spaces, as our data is space-separated (no pun intended)
-		reg := regexp.MustCompile("\\s+")
-		// Split our returned data into a string array
-		data := reg.Split(buf.String(), -1)
-		// Now we just need to get the last couple of groups of data. The last result will always be empty, so it's ignored
-
-		results["IonTemperature"], _ = strconv.ParseFloat(data[len(data)-2], 64)
-		results["Speed"], _ = strconv.ParseFloat(data[len(data)-3], 64)
-		results["Density"], _ = strconv.ParseFloat(data[len(data)-4], 64)
-		return nil
-	})
-
-	return results
+func extractData(r io.Reader) []string {
+	//
+	// Make a new buffer
+	buf := new(bytes.Buffer)
+	// And read the data from the file
+	buf.ReadFrom(r)
+	// This regex searches for consecutive spaces, as our data is space-separated (no pun intended)
+	reg := regexp.MustCompile("\\s+")
+	// Split our returned data into a string array
+	return reg.Split(buf.String(), -1)
 }
 
 // Check compares alert and warning thresholds defined above, and returns a value between 0 and 2, based on what was matched.
